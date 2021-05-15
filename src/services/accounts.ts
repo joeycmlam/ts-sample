ed72c65205653e7d9f4dfc6b4f2066b9dff510d1
joeylam <joey.cm.lam@gmail.com>
2021-05-16 01:11:56 +0800

    refector accounts class

--- src/services/accounts.ts
+++ src/services/accounts.ts
@@ -1,9 +1,16 @@
-import {accountItem} from "../models/iAccount";
-import {enumAccountStatus, enumAccountTypes} from "../models/iAccount";
+import {accountItem} from "../models/IAccount";
+import {enumAccountStatus, enumAccountTypes} from "../models/IAccount";
 
 class accounts {
     private _itemAccounts: accountItem[];
 
+    public addAccount(_account: accountItem): void {
+
+    }
+
+    public getAccount(_accountNo: string): accountItem {
+        return null;
+    }
 
     static instance = new accounts();
 }
