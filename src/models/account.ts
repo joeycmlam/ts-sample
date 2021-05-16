07b16f40e014d20b4e3d97a1bd62886088c69f75
joeylam <joey.cm.lam@gmail.com>
2021-05-16 17:26:27 +0800

    add test folder

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,39 +1,43 @@
 import {enumAccountStatus, enumAccountTypes} from "./IAccount";
 
 class Account  {
-    private accountNo: string;
-    private accountType: enumAccountTypes
-    private accountStatus: enumAccountStatus;
-    private dateCreated: Date;
+
+
+
+    private _accountNo: string = '';
+    private _accountType: enumAccountTypes;
+    private _accountStatus: enumAccountStatus = enumAccountStatus.DRAFT;
+    private _dateCreated: Date;
+
+    public get accountStatus () {
+        return this._accountStatus;
+    }
+
 
     getAccount(): Account {
         return null;
     }
 
     constructor (_accountNo: string, _accountType: enumAccountTypes) {
-        this.accountNo = _accountNo;
-        this.accountType = _accountType;
-        this.accountStatus = enumAccountStatus.DRAFT;
-        this.dateCreated = new Date();
-    }
-
-    getStatus(): enumAccountStatus {
-            return this.accountStatus;
+        this._accountNo = _accountNo;
+        this._accountType = _accountType;
+        this._accountStatus = enumAccountStatus.DRAFT;
+        this._dateCreated = new Date();
     }
 
     ackbyOps() {
-        this.accountStatus = enumAccountStatus.PENDING;
+        this._accountStatus = enumAccountStatus.PENDING;
     }
 
     closeAccount () {
-        this.accountStatus = enumAccountStatus.CLOSE;
+        this._accountStatus = enumAccountStatus.CLOSE;
     }
 }
 
 let a: Account = new Account('1', enumAccountTypes.TRADE_BASIS);
 
-console.log(`the status is ${a.getStatus()}`);
+console.log(`this is the status ${a.accountStatus}`)
 
 a.ackbyOps();
 
-console.log(`the status is changed to ${a.getStatus()}`);
+console.log(`this is the latest status ${a.accountStatus}`)
